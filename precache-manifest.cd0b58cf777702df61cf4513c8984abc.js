71fca984934a22998f1a7f924decebd23176cd74
Danny Reyes <reyesdmusic@gmail.com>
2020-07-01 14:19:52 -0400

    Updates

---
+++ precache-manifest.cd0b58cf777702df61cf4513c8984abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6218748ada7cf7cacf3428b8913cc071",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "3efcce29bc1b99b68b4e",
+    "url": "/profile/static/css/main.aebacc05.chunk.css"
+  },
+  {
+    "revision": "3d53bca13d34db73a080",
+    "url": "/profile/static/js/2.f27ea237.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.f27ea237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3efcce29bc1b99b68b4e",
+    "url": "/profile/static/js/main.8c2a91ec.chunk.js"
+  },
+  {
+    "revision": "46162f4a066cfd987408",
+    "url": "/profile/static/js/runtime-main.aa94450d.js"
+  }
+]);
