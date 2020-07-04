338adbe87490dfe5e2561ddaf2d8cbbb50947271
Danny Reyes <reyesdmusic@gmail.com>
2020-07-04 18:19:08 -0400

    Updates

---
+++ precache-manifest.c6c5ca2726412cfed05b0f1e11f25bab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f7d3e6cae5a360f7826382f6c212fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29fa6fa93763aa6813a4",
+    "url": "/portfolio/static/css/main.7a6f9f91.chunk.css"
+  },
+  {
+    "revision": "aec0133262cf600498c1",
+    "url": "/portfolio/static/js/2.8997e714.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.8997e714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fa6fa93763aa6813a4",
+    "url": "/portfolio/static/js/main.eb4b59e9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "82dd9a6b3e4a4b753680e1f5ecefc442",
+    "url": "/portfolio/static/media/budget-tracker-ui.82dd9a6b.png"
+  },
+  {
+    "revision": "e765ac7dfa35efd4873b807cef5addc2",
+    "url": "/portfolio/static/media/cli.e765ac7d.png"
+  },
+  {
+    "revision": "08823bc9e8c139f3666972f3f10cb3fc",
+    "url": "/portfolio/static/media/danny_reyes_resume2.08823bc9.pdf"
+  },
+  {
+    "revision": "9efc88efcfd1757ab97092ce6ae1b68f",
+    "url": "/portfolio/static/media/employee-directory-ui.9efc88ef.png"
+  },
+  {
+    "revision": "f963bc5e9b204b0555273507cadc8490",
+    "url": "/portfolio/static/media/profile-pic.f963bc5e.jpg"
+  },
+  {
+    "revision": "23f1e63302843ef1d7503f2bfa16da0b",
+    "url": "/portfolio/static/media/profilelogo.23f1e633.png"
+  },
+  {
+    "revision": "27c9aa7afe3779137b865d6d3c513e1c",
+    "url": "/portfolio/static/media/reeledIn-ui.27c9aa7a.png"
+  },
+  {
+    "revision": "9d57c53ee13c86c2ef8d1f240a40a4d4",
+    "url": "/portfolio/static/media/weather-dashboard-ui.9d57c53e.png"
+  },
+  {
+    "revision": "b7a441592fe404c2e6a6aff10c0e56bf",
+    "url": "/portfolio/static/media/workout-tracker-ui.b7a44159.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d25cece1b4eb4502fb52ad412f09b5c8.js"
+  "/portfolio/precache-manifest.c6c5ca2726412cfed05b0f1e11f25bab.js"
 );
 
 self.addEventListener('message', (event) => {
